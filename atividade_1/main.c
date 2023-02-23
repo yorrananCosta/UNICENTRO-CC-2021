ec8bd891b299a7539d0cecf9e8d6b1fc7a7c8b02
yorrananCosta <yorranan.almeida@gmail.com>
2023-02-23 18:02:29 -0300

    #9 - created new branch SO and added a file

---
+++ atividade_1/main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <phtread.h>
+#include <string.h>
+#include <stdlib.h>
+
+typedef struct Client
+{
+    int id;
+    string nome;
+    float balance;
+}
+
+void create_file ()
+{
+    
+}
+
+int main ()
+{
+    FILE *clients_file = NULL;
+
+    if(*clients_file == NULL)
+        fop
+}
